fcfe6463f2dbf31daa5252d2cfffdf0674ab1bf7
Ridenour <ridenou2@illinois.edu>
2017-03-11 19:30:44 -0600

    cp1 working yay

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -10,6 +10,7 @@
 #include "idt.h"
 #include "keyboard.h"
 #include "rtc.h"
+#include "paging.h"
 
 /* Macros. */
 /* Check if the bit BIT in FLAGS is set. */
@@ -167,6 +168,7 @@ entry (unsigned long magic, unsigned long addr)
 
 	/* Initialize devices, memory, filesystem, enable device interrupts on the
 	 * PIC, any other initialization stuff... */
+	init_paging();
 	
 	/* Do not enable the following until after you have set up your
 	 * IDT correctly otherwise QEMU will triple fault and simple close
