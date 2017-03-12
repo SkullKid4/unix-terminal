2f1140d57153d8d4042d5b0c384ba08f321e0163
Meyer <wemeyer2@illinois.edu>
2017-03-11 19:21:21 -0600

    fixed ) bug

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,12 +1,15 @@
 #ifndef PAGING_H
 #define PAGING_H
 
+#include "lib.h"
+
 #define NUM_PAGE_DIRECTORIES 1024
 #define NUM_PAGE_TABLES 1024
 #define FOUR_KB	4096
 
 
 void init_paging(void);
+
 void enable_Paging(uint32_t page_directory1);
 
 #endif
