a5a0444667641a888bb0b13edc9707a4cf2c40ac
Meyer <meyer32@illinois.edu>
2017-03-26 16:30:49 -0500

    pushing final rtc stuff with comments

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -6,6 +6,7 @@
 #include "syscall.h"
 
 volatile int rtc_interrupt;
+volatile unsigned rtc_lock = 0;
 /*
 rtc_init()
   Input: none
@@ -19,6 +20,7 @@ void rtc_init(){
   outb(0x8B, RTC_REGISTER_PORT); // select Register B again
   outb(prev | 0x40, RTC_DATA_PORT); // set bit 6 of Register B
   SET_IDT_ENTRY(idt[RTC_IDT_IDX], (rtc_handler));
+  enable_irq(RTC_IRQ);
 }
 
 /*
@@ -29,47 +31,65 @@ rtc_handler()
   to make sure interrupts will happen again.
 */
 void rtc_handler(){
-  cli();
+  if (rtc_lock == 0) {
+    rtc_lock =1;
+    cli();
   /* write EOI */
-  send_eoi(RTC_IRQ);
+    send_eoi(RTC_IRQ);
   //test_interrupts();
-  outb(0x0C, RTC_REGISTER_PORT); //select register C
-  inb(RTC_DATA_PORT); // read from register C to enable more interrupts.
-  rtc_interrupt = 1;
-  sti();
+    outb(0x0C, RTC_REGISTER_PORT); //select register C
+    inb(RTC_DATA_PORT); // read from register C to enable more interrupts.
+    rtc_interrupt = 1; // set interrupt flag
+    sti();
+    rtc_lock = 0;
+  }
 }
-int rtc_open(void) {
-  set_rate(2);
+
+// Open rtc sets the rate of the rtc to 2 hertz
+int32_t rtc_open(void) {
+  while (rtc_lock == 1)
+  {
+  }
+  rtc_lock = 1;
+  (void)set_rate(INITIAL_RATE);
+  rtc_lock = 0;
   return 0;
 }
 
-int rtc_close(void) {
+// rtc_close. Just returns 0.
+int32_t rtc_close(void) {
   return 0;
 }
 
-int rtc_read(void) {
-  int x = 3;
-  //rtc_interrupt = 0;
-  //printf ("waiting for interrupt");
+// rtc_read. waits for the next rtc interrupt.
+// spins until that interrupt occurs
+int32_t rtc_read(void) {
+  int32_t x = 3;
   while (!rtc_interrupt){
   }
-  //printf("got interrupt\n");
   x++;
   rtc_interrupt = 0;
-  //printf("cleared flag");
   return 0;
 }
-int rtc_write(int * buf, int nbytes) {
+
+
+int rtc_write(int32_t * buf, int32_t nbytes) {
+  cli();
   if (nbytes != 4 || buf == NULL)
     return -1;
-  int freq = *buf;
+  int32_t freq = *buf;
   return set_rate(freq);
+  sti();
 }
-int set_rate(int freq) {
+
+// Sets the rate of the RTC. Only powers of 2 Hertz of to 1024 Hz are valid
+// if an invalid rate is requested, no change is made.
+int32_t set_rate(int32_t freq) {
   /* Save old value of Reg A*/
   int8_t freq_const;
   outb(REGISTER_A, RTC_REGISTER_PORT);
   int8_t a_old = inb(RTC_DATA_PORT);
+  // change frequency constant
   switch (freq) {
     case 1024:
       freq_const = HZ_1024; 
@@ -105,32 +125,44 @@ int set_rate(int freq) {
       freq_const = HZ_0; 
       break;
     default:
-      return -1;
+      return FREQ_SUCCESS;
     }
+    // output frequency constant to register
     outb(REGISTER_A, RTC_REGISTER_PORT);
     outb((0xF0 & a_old) | freq_const, RTC_DATA_PORT);
     return FREQ_SUCCESS; // Return 4 on success 
 }
 
+
+/* Function for testing the rtc system calls
+  it open the rtc, setting the initial frequence to 2 HZ.
+  Then, it prints the letter 'l' every time the RTC sends an interrupt.
+*/
 void test_rtc(void) {
+  cli();
   rtc_open();
-  char * buf;
-  *buf = 'l';
-  //printf("opened rtc");
+  sti();
+  char buf[1];
+  buf[0] = 'l';
   while (1){
     rtc_read();
-    //printf("read rtc");
     write(STDOUT, buf, 1);
   }
 }
+
+/* Changes the frequency of the rtc. every time this is called, the frequency doubles,
+  except at 1024 hz, it goes to 0, and at 0 hz, it goes to 2 hz
+*/
 void toggle_freq(void) {
   int8_t freq_const;
   outb(REGISTER_A, RTC_REGISTER_PORT);
   int8_t a_old = inb(RTC_DATA_PORT);
+  // Change old frequency value
   if (a_old != 0)
     freq_const = a_old - 1;
   else 
     freq_const = 0x0F;
+  // output the new frequence constant to 
   outb(REGISTER_A, RTC_REGISTER_PORT);
   outb((0xF0 & a_old) | freq_const, RTC_DATA_PORT);
 
