0ce42fee3721d58c4cb28165c86ad0cfea7a012c
Meyer <wemeyer2@illinois.edu>
2017-03-26 17:32:07 -0500

    commests for syscalls done

--- student-distrib/syscall.h
+++ student-distrib/syscall.h
@@ -6,10 +6,16 @@
 #define STDIN	0
 #define STDOUT	1
 
+/*dispacture for the system calls*/
 extern void system_call_handler();
+
+/*function for reading data to buffers*/
 extern int32_t read(int32_t fd, void* buf, int32_t nbytes);
+/*function to writing to buffers*/
 extern int32_t write(int32_t fd, const void* buf, int32_t nbytes);
+/*opens files*/
 extern int32_t open(const uint8_t* filename);
+/*closes files*/
 extern int32_t close(int32_t fd);
 
 
