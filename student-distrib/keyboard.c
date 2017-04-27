37d8dcbaabd1d890176edd6e971267f1ccf1be18
Ridenour <ridenou2@illinois.edu>
2017-04-27 15:32:01 -0500

    started multiple terminals

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -14,6 +14,7 @@ volatile unsigned lock = 0;       //used to lock the thread when writing keyboar
 volatile unsigned shift = 0;      //2a or 36 on press;
 volatile unsigned caps = 0;
 volatile unsigned ctrl = 0;
+volatile unsigned alt = 0;
 
 
 volatile uint8_t keyboard_idx;
@@ -167,7 +168,7 @@ void keyboard_handler(){
   
     if (status & 0x01) {                    //if the status is set, get the code from the keyboard port
       keycode = inb(KEYBOARD_DATA_PORT);
-      //printf("Keycode is: %d", keycode);
+      //printf("Keycode is: 0x%x", keycode);
       if(keycode == BACKSPACE && ((screen_x + screen_y) != 0)){
         handle_backspace();
         sti();
@@ -205,6 +206,20 @@ void keyboard_handler(){
       }
       if(keycode == CTRL_DOWN){
         ctrl = 1;
+        sti();
+        lock = 0;
+        return;
+      }
+
+      if(keycode == ALT_DOWN) {
+        alt = 1;
+        sti();
+        lock = 0;
+        return;
+      }
+
+      if(keycode == ALT_UP) {
+        alt = 0;
         sti();
         lock = 0;
         return;
@@ -330,6 +345,21 @@ void keyboard_handler(){
     lock=0;
     return;
   }
+
+  if(alt) {
+    switch(keycode) {
+      case F1:
+        switch_terminal(0);
+      case F2:
+        switch_terminal(1);
+      case F3:
+        switch_terminal(2);
+    }
+    sti();
+    lock = 0;
+    return;
+  }
+
       if(keyboard_idx == MAX_BUF_SIZE){
         sti();
         lock = 0;
