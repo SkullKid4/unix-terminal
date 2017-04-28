a51add719df7daf9e12efefa811659ae20dbd498
Meyer <wemeyer2@illinois.edu>
2017-04-27 21:39:33 -0500

    working on scheduler

--- student-distrib/scheduler.c
+++ student-distrib/scheduler.c
@@ -1,9 +1,8 @@
 #include "scheduler.h"
 
-void switch_task(registers_t *regs){
+void switch_task(){
    /* copy the saved registers into the current_proc structure */
-   memcpy(&current_proc->regs, regs, sizeof(registers_t));
- 
+	
    /* now go onto the next task - if there isn't one, go back to the start
       of the queue. */
    if (current_proc->next != NULL){
@@ -12,6 +11,6 @@ void switch_task(registers_t *regs){
 		current_proc = pqueue;
    }
    /* now hack the registers! */
-   memcpy(regs, &current_proc->regs, sizeof(registers_t));
+   //memcpy(regs, &current_proc->regs, sizeof(registers_t));
    //map?				switch_page_directory(current_proc->cr3);
 }
--- student-distrib/scheduler.h
+++ student-distrib/scheduler.h
@@ -6,14 +6,15 @@
 
 typedef struct registers_struct
 {
-	int32_t eax;
-	int32_t ebx;
-	int32_t ecx;
-	int32_t edx;
-	int32_t ebp;
 	int32_t edi;
 	int32_t esi;
+	int32_t user_esp;
+	int32_t ebp;
 	int32_t esp;
+	int32_t ebx;
+	int32_t edx;
+	int32_t ecx;
+	int32_t eax;
 } registers_t;
 
 typedef struct process_struct
@@ -23,10 +24,10 @@ typedef struct process_struct
     struct process_struct *next;
 } process_t;
 
-process_t *pqueue = NULL;
-process_t *current_proc = NULL;
+process_t *pqueue;
+process_t *current_proc;
 
-void switch_task(registers_t *regs);
+void switch_task();
 
 #endif
 
