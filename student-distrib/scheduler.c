e4dc6913dbfaeca429480b1b60d45853122aabf8
Meyer <wemeyer2@illinois.edu>
2017-04-28 20:57:43 -0500

    scheduler still broken

--- student-distrib/scheduler.c
+++ student-distrib/scheduler.c
@@ -6,28 +6,41 @@
 #include "pit.h"
 
 volatile uint8_t task_idx = 0;
+volatile uint8_t task_lock = 0;
 
 void switch_task(){
    /* copy the saved registers into the current_proc structure */
-   uint8_t temp_idx = task_idx;
-   uint8_t count =0;
-   send_eoi(PIT_IRQ);
-   /* now go onto the next task - if there isn't one, go back to the start
-      of the queue. */
-   while(task_array[task_idx].active == 0){
-      task_idx++;
-	  count++;
-      if(task_idx >= 6){
-         task_idx = 0;
+   if(task_lock == 0){
+      task_lock = 1;
+      cli();
+      uint8_t temp_idx = task_idx;
+      uint8_t count =0;
+      
+      /* now go onto the next task - if there isn't one, go back to the start
+         of the queue. */
+      while(task_array[task_idx].active == 0){
+         task_idx++;
+         count++;
+         if(task_idx >= 6){
+            task_idx = 0;
+         }
+        if(count>6){
+            sti();
+            task_lock = 0;
+            send_eoi(PIT_IRQ);
+            return;
+        }
       }
-	  if(count>6)
-		  return;
+      task_idx++;
+      
+      tss.esp0 = PHYS_FILE_START - EIGHT_KB * (task_array[task_idx].PID) - 4;
+      map(VIRTUAL_FILE_PAGE, PHYS_FILE_START + PHYS_FILE_OFFSET * task_array[task_idx].PID);
+      register int temp asm("esp");
+      task_array[temp_idx].ESP0 = temp;
+      sti();
+      task_lock = 0;
+      send_eoi(PIT_IRQ);
    }
-   
-   tss.esp0 = PHYS_FILE_START - EIGHT_KB * (task_array[task_idx].PID) - 4;
-   map(VIRTUAL_FILE_PAGE, PHYS_FILE_START + PHYS_FILE_OFFSET * task_array[task_idx].PID);
-   register int temp asm("esp");
-   task_array[temp_idx].ESP0 = temp;
 }
 
 void scheduler_init(){
